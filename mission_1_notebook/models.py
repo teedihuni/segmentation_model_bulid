cf9e5af839c70d19340e3e510485601bb0fa5362
teedihuni <dhlee132@gmail.com>
2022-12-21 13:35:12 +0000

    FCN model

---
+++ mission_1_notebook/models.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+class FCN8s(nn.Module):
+    def __init__(self, num_classes=21):
+        super(FCN8s, self).__init__()
+        
+        
+        
+        self.relu    = nn.ReLU(inplace=True)
+        
+        
+       
+
+    def forward(self, x):
+           
+        output = self.relu(x)
+        return output
